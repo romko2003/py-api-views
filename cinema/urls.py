0ef6a40e44754387db2dd8f6ab1f8ae349c751d5
= <=>
2025-08-14 23:53:30 +0300

    implement

--- cinema/urls.py
+++ cinema/urls.py
@@ -15,12 +15,16 @@ router.register(r"cinema_halls", CinemaHallViewSet, basename="cinema_hall")
 
 urlpatterns = [
     # Genres (APIView)
-    path("api/cinema/genres/", GenreListCreateAPIView.as_view(), name="genre-list"),
-    path("api/cinema/genres/<int:pk>/", GenreDetailAPIView.as_view(), name="genre-detail"),
+    path("api/cinema/genres/", GenreListCreateAPIView.as_view(),
+         name="genre-list"),
+    path("api/cinema/genres/<int:pk>/", GenreDetailAPIView.as_view(),
+         name="genre-detail"),
 
     # Actors (GenericAPIView)
-    path("api/cinema/actors/", ActorListCreateAPIView.as_view(), name="actor-list"),
-    path("api/cinema/actors/<int:pk>/", ActorDetailAPIView.as_view(), name="actor-detail"),
+    path("api/cinema/actors/", ActorListCreateAPIView.as_view(),
+         name="actor-list"),
+    path("api/cinema/actors/<int:pk>/", ActorDetailAPIView.as_view(),
+         name="actor-detail"),
 
     # ViewSets (CinemaHall + Movie) via router
     path("api/cinema/", include(router.urls)),
